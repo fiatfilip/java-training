3a4848f65ae28e741c8d09b35198edd945d83e7b
Valeriu Cujba <cujba.valeriu1@gmail.com>
2022-04-04 18:29:05 +0300

    Tema 1 Challenge-uri

--- _1_basics/src/main/java/code/_4_student_effort/Main.java
+++ _1_basics/src/main/java/code/_4_student_effort/Main.java
@@ -1,10 +1,23 @@
 package code._4_student_effort;
 
+
+import code._4_student_effort.FizzBuzzChallenge.FizzBuzz;
+import code._4_student_effort.FooBarQix.FooBarQix;
+
 public class Main {
 
   public static void main(String[] args) {
     //TODO put your code changes in here
-    //comment incercare
-    //Incercare commit 2.0
+    //FizzBuzz challenge
+
+
+//    FizzBuzz fizzBuzz = new FizzBuzz();
+//    fizzBuzz.fizzbuzz();
+//    fizzBuzz.fizzBuzz2();
+
+    // FooBarQix challenge
+    FooBarQix foo = new FooBarQix();
+    System.out.println(foo.compute2(101));
+
   }
 }
